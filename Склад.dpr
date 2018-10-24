3db19de14defc18074ca0ca0ed2e39253f74f9d9
Vladimir <voffka.kv@gmail.com>
2018-10-24 11:56:39 +0600

    123

--- Склад.dpr
+++ Склад.dpr
@@ -3,8 +3,7 @@ program Склад;
 uses
   Vcl.Forms,
   UnitMain in 'UnitMain.pas' {Form1},
-  UnitAdd in 'UnitAdd.pas' {Add},
-  UnitConnection in 'UnitConnection.pas' {Form2};
+  UnitAdd in 'UnitAdd.pas' {Add};
 
 {$R *.res}
 
@@ -13,6 +12,5 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
   Application.CreateForm(TAdd, Add);
-  Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
